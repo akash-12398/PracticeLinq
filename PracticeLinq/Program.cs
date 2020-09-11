0fdc10ffd503dbf803e03484f33039bac443d975
akash-12398 <napinderbajwa@gmail.com>
2020-09-11 11:58:16 -0500

    PracticeLinq

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+      
+            List<string> videoGames = new List<string>
+            { "AngryBirds", "Fortnite", "Pokemon", "Mario" };
+
+            List<int> videogameslength = new List<int>
+            { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            
+
+            var length =
+                videoGames.Select(videogames => videogames.Length);
+            
+            
+            foreach (var games in length)
+
+            {
+                Console.WriteLine(games);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+              List<string> videoGames = new List<string>
+
+              {"AngryBirds", "Fortnite", "Pokemon", "Mario" };
+
+            Orderby<string> LongVideoGames =
+                 videoGames.Where(game => game.Lenth.Contains("5"));
+
+            foreach (var i in videoGames)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
